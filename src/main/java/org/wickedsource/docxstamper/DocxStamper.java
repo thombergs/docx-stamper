c054ee3e4df6e188dfd8819324811bb51893dd47
Youssouf Naciri <youssouf.naciri@external.totalenergies.com>
2022-08-18 17:54:21 +0200

    calling docx stamper for subtemplate :)

--- src/main/java/org/wickedsource/docxstamper/DocxStamper.java
+++ src/main/java/org/wickedsource/docxstamper/DocxStamper.java
@@ -34,138 +34,138 @@ import java.util.Map;
  */
 public class DocxStamper<T> {
 
-  private PlaceholderReplacer<T> placeholderReplacer;
+    private PlaceholderReplacer<T> placeholderReplacer;
 
-  private CommentProcessorRegistry commentProcessorRegistry;
+    private CommentProcessorRegistry commentProcessorRegistry;
 
-  private TypeResolverRegistry typeResolverRegistry;
+    private TypeResolverRegistry typeResolverRegistry;
 
-  private DocxStamperConfiguration config = new DocxStamperConfiguration();
+    private DocxStamperConfiguration config = new DocxStamperConfiguration();
 
-  public DocxStamper() {
-    initFields();
-  }
-
-  public DocxStamper(DocxStamperConfiguration config) {
-    this.config = config;
-    initFields();
-  }
-
-  private void initFields() {
-    typeResolverRegistry = new TypeResolverRegistry(new FallbackResolver());
-    typeResolverRegistry.registerTypeResolver(Image.class, new ImageResolver());
-    typeResolverRegistry.registerTypeResolver(Date.class, new DateResolver("dd.MM.yyyy"));
-    for (Map.Entry<Class<?>, ITypeResolver> entry : config.getTypeResolvers().entrySet()) {
-      typeResolverRegistry.registerTypeResolver(entry.getKey(), entry.getValue());
+    public DocxStamper() {
+        initFields();
     }
 
-    ExpressionResolver expressionResolver = new ExpressionResolver(config.getEvaluationContextConfigurer());
-    placeholderReplacer = new PlaceholderReplacer<>(typeResolverRegistry, config.getLineBreakPlaceholder());
-    placeholderReplacer.setExpressionResolver(expressionResolver);
-    placeholderReplacer.setLeaveEmptyOnExpressionError(config.isLeaveEmptyOnExpressionError());
-    placeholderReplacer.setReplaceNullValues(config.isReplaceNullValues());
-
-    commentProcessorRegistry = new CommentProcessorRegistry(placeholderReplacer);
-    commentProcessorRegistry.setExpressionResolver(expressionResolver);
-    commentProcessorRegistry.setFailOnInvalidExpression(config.isFailOnUnresolvedExpression());
-    commentProcessorRegistry.registerCommentProcessor(IRepeatProcessor.class, new RepeatProcessor(typeResolverRegistry, expressionResolver));
-    commentProcessorRegistry.registerCommentProcessor(IParagraphRepeatProcessor.class, new ParagraphRepeatProcessor(typeResolverRegistry, expressionResolver));
-    commentProcessorRegistry.registerCommentProcessor(IRepeatDocPartProcessor.class, new RepeatDocPartProcessor(typeResolverRegistry, expressionResolver));
-    commentProcessorRegistry.registerCommentProcessor(IDisplayIfProcessor.class, new DisplayIfProcessor());
-    commentProcessorRegistry.registerCommentProcessor(IReplaceWithProcessor.class,
-            new ReplaceWithProcessor());
-    for (Map.Entry<Class<?>, ICommentProcessor> entry : config.getCommentProcessors().entrySet()) {
-      commentProcessorRegistry.registerCommentProcessor(entry.getKey(), entry.getValue());
+    public DocxStamper(DocxStamperConfiguration config) {
+        this.config = config;
+        initFields();
     }
-  }
 
-  /**
-   * <p>
-   * Reads in a .docx template and "stamps" it into the given OutputStream, using the specified context object to
-   * fill out any expressions it finds.
-   * </p>
-   * <p>
-   * In the .docx template you have the following options to influence the "stamping" process:
-   * </p>
-   * <ul>
-   * <li>Use expressions like ${name} or ${person.isOlderThan(18)} in the template's text. These expressions are resolved
-   * against the contextRoot object you pass into this method and are replaced by the results.</li>
-   * <li>Use comments within the .docx template to mark certain paragraphs to be manipulated. </li>
-   * </ul>
-   * <p>
-   * Within comments, you can put expressions in which you can use the following methods by default:
-   * </p>
-   * <ul>
-   * <li><em>displayParagraphIf(boolean)</em> to conditionally display paragraphs or not</li>
-   * <li><em>displayTableRowIf(boolean)</em> to conditionally display table rows or not</li>
-   * <li><em>displayTableIf(boolean)</em> to conditionally display whole tables or not</li>
-   * <li><em>repeatTableRow(List&lt;Object&gt;)</em> to create a new table row for each object in the list and resolve expressions
-   * within the table cells against one of the objects within the list.</li>
-   * </ul>
-   * <p>
-   * If you need a wider vocabulary of methods available in the comments, you can create your own ICommentProcessor
-   * and register it via getCommentProcessorRegistry().addCommentProcessor().
-   * </p>
-   *
-   * @param template    the .docx template.
-   * @param contextRoot the context root object against which all expressions found in the template are evaluated.
-   * @param out         the output stream in which to write the resulting .docx document.
-   * @throws DocxStamperException in case of an error.
-   */
-  public void stamp(InputStream template, T contextRoot, OutputStream out) throws DocxStamperException {
-    try {
-      WordprocessingMLPackage document = WordprocessingMLPackage.load(template);
-      stamp(document, contextRoot, out);
-    } catch (DocxStamperException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new DocxStamperException(e);
+    private void initFields() {
+        typeResolverRegistry = new TypeResolverRegistry(new FallbackResolver());
+        typeResolverRegistry.registerTypeResolver(Image.class, new ImageResolver());
+        typeResolverRegistry.registerTypeResolver(Date.class, new DateResolver("dd.MM.yyyy"));
+        for (Map.Entry<Class<?>, ITypeResolver> entry : config.getTypeResolvers().entrySet()) {
+            typeResolverRegistry.registerTypeResolver(entry.getKey(), entry.getValue());
+        }
+
+        ExpressionResolver expressionResolver = new ExpressionResolver(config.getEvaluationContextConfigurer());
+        placeholderReplacer = new PlaceholderReplacer<>(typeResolverRegistry, config.getLineBreakPlaceholder());
+        placeholderReplacer.setExpressionResolver(expressionResolver);
+        placeholderReplacer.setLeaveEmptyOnExpressionError(config.isLeaveEmptyOnExpressionError());
+        placeholderReplacer.setReplaceNullValues(config.isReplaceNullValues());
+
+        commentProcessorRegistry = new CommentProcessorRegistry(placeholderReplacer);
+        commentProcessorRegistry.setExpressionResolver(expressionResolver);
+        commentProcessorRegistry.setFailOnInvalidExpression(config.isFailOnUnresolvedExpression());
+        commentProcessorRegistry.registerCommentProcessor(IRepeatProcessor.class, new RepeatProcessor(typeResolverRegistry, expressionResolver));
+        commentProcessorRegistry.registerCommentProcessor(IParagraphRepeatProcessor.class, new ParagraphRepeatProcessor(typeResolverRegistry, expressionResolver));
+        commentProcessorRegistry.registerCommentProcessor(IRepeatDocPartProcessor.class, new RepeatDocPartProcessor(typeResolverRegistry, expressionResolver, config));
+        commentProcessorRegistry.registerCommentProcessor(IDisplayIfProcessor.class, new DisplayIfProcessor());
+        commentProcessorRegistry.registerCommentProcessor(IReplaceWithProcessor.class,
+                new ReplaceWithProcessor());
+        for (Map.Entry<Class<?>, ICommentProcessor> entry : config.getCommentProcessors().entrySet()) {
+            commentProcessorRegistry.registerCommentProcessor(entry.getKey(), entry.getValue());
+        }
     }
-  }
 
-  /**
-   * Same as stamp(InputStream, T, OutputStream) except that you may pass in a DOCX4J document as a template instead
-   * of an InputStream.
-   *
-   * @param document    the .docx template.
-   * @param contextRoot the context root object against which all expressions found in the template are evaluated.
-   * @param out         the output stream in which to write the resulting .docx document.
-   * @throws DocxStamperException in case of an error.
-   */
-  public void stamp(WordprocessingMLPackage document, T contextRoot, OutputStream out) throws DocxStamperException {
-    try {
-      ProxyBuilder<T> proxyBuilder = addCustomInterfacesToContextRoot(contextRoot, this.config.getExpressionFunctions());
-      processComments(document, proxyBuilder);
-      replaceExpressions(document, proxyBuilder);
-      document.save(out);
-      commentProcessorRegistry.reset();
-    } catch (DocxStamperException e) {
-      throw e;
-    } catch (Exception e) {
-      throw new DocxStamperException(e);
+    /**
+     * <p>
+     * Reads in a .docx template and "stamps" it into the given OutputStream, using the specified context object to
+     * fill out any expressions it finds.
+     * </p>
+     * <p>
+     * In the .docx template you have the following options to influence the "stamping" process:
+     * </p>
+     * <ul>
+     * <li>Use expressions like ${name} or ${person.isOlderThan(18)} in the template's text. These expressions are resolved
+     * against the contextRoot object you pass into this method and are replaced by the results.</li>
+     * <li>Use comments within the .docx template to mark certain paragraphs to be manipulated. </li>
+     * </ul>
+     * <p>
+     * Within comments, you can put expressions in which you can use the following methods by default:
+     * </p>
+     * <ul>
+     * <li><em>displayParagraphIf(boolean)</em> to conditionally display paragraphs or not</li>
+     * <li><em>displayTableRowIf(boolean)</em> to conditionally display table rows or not</li>
+     * <li><em>displayTableIf(boolean)</em> to conditionally display whole tables or not</li>
+     * <li><em>repeatTableRow(List&lt;Object&gt;)</em> to create a new table row for each object in the list and resolve expressions
+     * within the table cells against one of the objects within the list.</li>
+     * </ul>
+     * <p>
+     * If you need a wider vocabulary of methods available in the comments, you can create your own ICommentProcessor
+     * and register it via getCommentProcessorRegistry().addCommentProcessor().
+     * </p>
+     *
+     * @param template    the .docx template.
+     * @param contextRoot the context root object against which all expressions found in the template are evaluated.
+     * @param out         the output stream in which to write the resulting .docx document.
+     * @throws DocxStamperException in case of an error.
+     */
+    public void stamp(InputStream template, T contextRoot, OutputStream out) throws DocxStamperException {
+        try {
+            WordprocessingMLPackage document = WordprocessingMLPackage.load(template);
+            stamp(document, contextRoot, out);
+        } catch (DocxStamperException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DocxStamperException(e);
+        }
     }
-  }
 
-  private ProxyBuilder<T> addCustomInterfacesToContextRoot(T contextRoot, Map<Class<?>, Object> interfacesToImplementations) {
-    ProxyBuilder<T> proxyBuilder = new ProxyBuilder<T>()
-            .withRoot(contextRoot);
-    if (interfacesToImplementations.isEmpty()) {
-      return proxyBuilder;
+    /**
+     * Same as stamp(InputStream, T, OutputStream) except that you may pass in a DOCX4J document as a template instead
+     * of an InputStream.
+     *
+     * @param document    the .docx template.
+     * @param contextRoot the context root object against which all expressions found in the template are evaluated.
+     * @param out         the output stream in which to write the resulting .docx document.
+     * @throws DocxStamperException in case of an error.
+     */
+    public void stamp(WordprocessingMLPackage document, T contextRoot, OutputStream out) throws DocxStamperException {
+        try {
+            ProxyBuilder<T> proxyBuilder = addCustomInterfacesToContextRoot(contextRoot, this.config.getExpressionFunctions());
+            processComments(document, proxyBuilder);
+            replaceExpressions(document, proxyBuilder);
+            document.save(out);
+            commentProcessorRegistry.reset();
+        } catch (DocxStamperException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new DocxStamperException(e);
+        }
     }
-    for (Map.Entry<Class<?>, Object> entry : interfacesToImplementations.entrySet()) {
-      Class<?> interfaceClass = entry.getKey();
-      Object implementation = entry.getValue();
-      proxyBuilder.withInterface(interfaceClass, implementation);
+
+    private ProxyBuilder<T> addCustomInterfacesToContextRoot(T contextRoot, Map<Class<?>, Object> interfacesToImplementations) {
+        ProxyBuilder<T> proxyBuilder = new ProxyBuilder<T>()
+                .withRoot(contextRoot);
+        if (interfacesToImplementations.isEmpty()) {
+            return proxyBuilder;
+        }
+        for (Map.Entry<Class<?>, Object> entry : interfacesToImplementations.entrySet()) {
+            Class<?> interfaceClass = entry.getKey();
+            Object implementation = entry.getValue();
+            proxyBuilder.withInterface(interfaceClass, implementation);
+        }
+        return proxyBuilder;
     }
-    return proxyBuilder;
-  }
 
-  private void replaceExpressions(WordprocessingMLPackage document, ProxyBuilder<T> proxyBuilder) {
-    placeholderReplacer.resolveExpressions(document, proxyBuilder);
-  }
+    private void replaceExpressions(WordprocessingMLPackage document, ProxyBuilder<T> proxyBuilder) {
+        placeholderReplacer.resolveExpressions(document, proxyBuilder);
+    }
 
-  private void processComments(final WordprocessingMLPackage document, ProxyBuilder<T> proxyBuilder) {
-    commentProcessorRegistry.runProcessors(document, proxyBuilder);
-  }
+    private void processComments(final WordprocessingMLPackage document, ProxyBuilder<T> proxyBuilder) {
+        commentProcessorRegistry.runProcessors(document, proxyBuilder);
+    }
 
 }
