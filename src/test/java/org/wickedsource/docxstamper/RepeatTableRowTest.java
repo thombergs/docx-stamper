9035f635133395aa3096fcfb76be7de390b8b05a
Joseph Verron <joseph@verron.pro>
2023-07-11 00:54:13 +0200

    Update tests setup

--- src/test/java/org/wickedsource/docxstamper/RepeatTableRowTest.java
+++ src/test/java/org/wickedsource/docxstamper/RepeatTableRowTest.java
@@ -1,89 +1,60 @@
 package org.wickedsource.docxstamper;
 
+import lombok.Getter;
 import org.docx4j.openpackaging.exceptions.Docx4JException;
-import org.docx4j.wml.P;
-import org.docx4j.wml.Tbl;
-import org.docx4j.wml.Tc;
-import org.docx4j.wml.Tr;
 import org.junit.jupiter.api.Test;
-import org.wickedsource.docxstamper.util.DocumentUtil;
-import org.wickedsource.docxstamper.util.ParagraphWrapper;
 import pro.verron.docxstamper.utils.TestDocxStamper;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 public class RepeatTableRowTest {
-	@Test
-	public void test() throws Docx4JException, IOException {
-		var context = new Characters();
-		context.getCharacters().add(new Character("Homer Simpson", "Dan Castellaneta"));
-		context.getCharacters().add(new Character("Marge Simpson", "Julie Kavner"));
-		context.getCharacters().add(new Character("Bart Simpson", "Nancy Cartwright"));
-		context.getCharacters().add(new Character("Kent Brockman", "Harry Shearer"));
-		context.getCharacters().add(new Character("Disco Stu", "Hank Azaria"));
-		context.getCharacters().add(new Character("Krusty the Clown", "Dan Castellaneta"));
-		InputStream template = getClass().getResourceAsStream("RepeatTableRowTest.docx");
+    @Test
+    public void test() throws Docx4JException, IOException {
+        var context = new Characters();
+        context.getCharacters().add(new Character("Homer Simpson", "Dan Castellaneta"));
+        context.getCharacters().add(new Character("Marge Simpson", "Julie Kavner"));
+        context.getCharacters().add(new Character("Bart Simpson", "Nancy Cartwright"));
+        context.getCharacters().add(new Character("Kent Brockman", "Harry Shearer"));
+        context.getCharacters().add(new Character("Disco Stu", "Hank Azaria"));
+        context.getCharacters().add(new Character("Krusty the Clown", "Dan Castellaneta"));
+        var template = getClass().getResourceAsStream("RepeatTableRowTest.docx");
 
-		var stamper = new TestDocxStamper<Characters>();
-		var document = stamper.stampAndLoad(template, context);
+        var actual = new TestDocxStamper<>().stampAndLoadAndExtract(template, context);
 
-		var rows = DocumentUtil.getTableRowsFromObject(document);
-		var tablesFromObject = DocumentUtil.streamElements(document, Tbl.class).toList();
-		assertEquals(1, tablesFromObject.size());
+        var expected = List.of(
+                "|Repeating Table Rows/|//rPr={},spacing=xxx",
+                "|List of Simpsons characters/b=true|//rPr={b=true},spacing=xxx",
+                "|Character name/b=true|//rPr={b=true}",
+                "|Voice /b=true||Actor/b=true|//rPr={b=true}",
+                "|Homer Simpson/|//rPr={}",
+                "|Dan Castellaneta/|//rPr={}",
+                "|Marge Simpson/|//rPr={}",
+                "|Julie Kavner/|//rPr={}",
+                "|Bart Simpson/|//rPr={}",
+                "|Nancy Cartwright/|//rPr={}",
+                "|Kent Brockman/|//rPr={}",
+                "|Harry Shearer/|//rPr={}",
+                "|Disco Stu/|//rPr={}",
+                "|Hank Azaria/|//rPr={}",
+                "|Krusty the Clown/|//rPr={}",
+                "|Dan Castellaneta/|//rPr={}",
+                "//rPr={}",
+                "|There are /||6/lang=de-DE|| characters in the above table./|//rPr={lang=de-DE},spacing=xxx"
+        );
+        assertIterableEquals(expected, actual);
+    }
 
-		var parentTableRows = DocumentUtil.streamElements(tablesFromObject.get(0), Tr.class).toList();
-		// 1 header row + 1 row per character in list
-		assertEquals(7, rows.size());
 
-		assertEquals(7, parentTableRows.size());
+    public record Character(String name, String actor) {
+    }
 
-		final List<Tc> cells = DocumentUtil.getTableCellsFromObject(document);
-
-		assertEquals("Homer Simpson", new ParagraphWrapper((P) cells.get(2).getContent().get(0)).getText());
-		assertEquals("Dan Castellaneta", new ParagraphWrapper((P) cells.get(3).getContent().get(0)).getText());
-		assertEquals("Marge Simpson", new ParagraphWrapper((P) cells.get(4).getContent().get(0)).getText());
-		assertEquals("Julie Kavner", new ParagraphWrapper((P) cells.get(5).getContent().get(0)).getText());
-		assertEquals("Bart Simpson", new ParagraphWrapper((P) cells.get(6).getContent().get(0)).getText());
-		assertEquals("Nancy Cartwright", new ParagraphWrapper((P) cells.get(7).getContent().get(0)).getText());
-		assertEquals("Kent Brockman", new ParagraphWrapper((P) cells.get(8).getContent().get(0)).getText());
-		assertEquals("Harry Shearer", new ParagraphWrapper((P) cells.get(9).getContent().get(0)).getText());
-		assertEquals("Disco Stu", new ParagraphWrapper((P) cells.get(10).getContent().get(0)).getText());
-		assertEquals("Hank Azaria", new ParagraphWrapper((P) cells.get(11).getContent().get(0)).getText());
-		assertEquals("Krusty the Clown", new ParagraphWrapper((P) cells.get(12).getContent().get(0)).getText());
-		assertEquals("Dan Castellaneta", new ParagraphWrapper((P) cells.get(13).getContent().get(0)).getText());
-	}
-
-	public static class Character {
-
-		private final String name;
-
-		private final String actor;
-
-		public Character(String name, String actor) {
-			this.name = name;
-			this.actor = actor;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public String getActor() {
-			return actor;
-		}
-	}
-
-	public class Characters {
-
-		private final List<Character> characters = new ArrayList<>();
-
-		public List<Character> getCharacters() {
-			return characters;
-		}
-	}
+    @Getter
+    public static class Characters {
+        private final List<Character> characters = new ArrayList<>();
+    }
 }
